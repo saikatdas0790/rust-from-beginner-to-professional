917da791664581adf72e3814507a4de80d61b889
Saikat Das <saikatdas0790@gmail.com>
2025-05-06 16:00:08 +0000

    Refactor main function to use for_each for handling incoming connections

--- hello_web/src/main.rs
+++ hello_web/src/main.rs
@@ -4,16 +4,15 @@ use std::{
 };
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
-
-    for stream in listener.incoming() {
-        handle_client(stream.unwrap());
-    }
+    TcpListener::bind("127.0.0.1:3000")
+        .unwrap()
+        .incoming()
+        .for_each(|stream| handle_client(stream.unwrap()));
 }
 
 fn handle_client(mut stream: TcpStream) {
-    stream.read(&mut [0; 1024]).unwrap();
     stream
-        .write(b"HTTP/1.1 200 OK\r\n\r\nHello, world!")
+        .read(&mut [0; 1024])
+        .and_then(|_| stream.write(b"HTTP/1.1 200 OK\r\n\r\nHello, world!"))
         .unwrap();
 }
